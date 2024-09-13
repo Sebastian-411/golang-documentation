8db0f435227cb0043e33ce0478fc762f32e1ee80
sebastian411 <jdiaz@vozy.co>
2024-09-13 13:49:52 -0500

    chore: Refactor generic slice printing function
    Refactor the `imprimirSlice` function in the `2.7-generics/main.go` file to improve code readability and maintainability. The function now uses consistent indentation and follows the recommended coding style. This change does not affect the functionality of the code.

--- 2.7-generics/main.go
+++ 2.7-generics/main.go
@@ -4,16 +4,15 @@ import "fmt"
 
 // Función genérica que imprime cualquier tipo de slice
 func imprimirSlice[T any](s []T) {
-    for _, v := range s {
-        fmt.Println(v)
-    }
+	for _, v := range s {
+		fmt.Println(v)
+	}
 }
 
 func main() {
-    // Uso de la función genérica
-    ints := []int{1, 2, 3}
-    strings := []string{"uno", "dos", "tres"}
+	ints := []int{1, 2, 3}
+	strings := []string{"uno", "dos", "tres"}
 
-    imprimirSlice(ints)
-    imprimirSlice(strings)
+	imprimirSlice(ints)
+	imprimirSlice(strings)
 }
